364f3fc23aa68c4b61fc72c3306593bfd106707b
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-12 13:56:39 -0200

    optimizeCSSAssetsPlugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,12 +1,22 @@
 const path = require('path');
 const babiliPlugin = require('babili-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin');
+const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 let plugins = [];
 plugins.push(new extractTextPlugin('styles.css'));
 
 if (process.env.NODE_ENV == 'production') {
     plugins.push(new babiliPlugin());
+    plugins.push(new optimizeCSSAssetsPlugin({
+        "cssProcessor": require('cssnano'),
+        "cssProcessorOptions": {
+            "discardComments": {
+                "removeAll": true
+            }
+        },
+        canPrint: true
+    }));
 }
 
 module.exports = {
